a895b5d0d11e6869f6c82c076491121a0883b70c
k-l-lambda <k.l.lambda@gmail.com>
2019-03-12 00:01:10 +0800

    index.vue: added features array.

--- front-end/index.vue
+++ front-end/index.vue
@@ -1,14 +1,45 @@
 <template>
 	<body>
-		hello.
-		<img src="/test?instantaneous" />
+		<img v-if="latentsBytes" :src="`/generate?psi=${psi}&latents=${latentsBytes}`" />
 	</body>
 </template>
 
 <script>
-	// btoa(String.fromCharCode.apply(null, new Uint8Array(new Float32Array(Array(512).fill(0)).buffer)))
 	export default {
 		name: "index",
+
+
+		data () {
+			return {
+				model: null,
+				latents_dimensions: null,
+				features: null,
+				psi: 0.7,
+			};
+		},
+
+
+		computed: {
+			latentsBytes () {
+				if (!this.features)
+					return null;
+
+				return btoa(String.fromCharCode.apply(null, new Uint8Array(new Float32Array(this.features).buffer)));
+			},
+		},
+
+
+		async mounted () {
+			window.__main = this;
+
+			const res = await fetch("/spec");
+			const spec = await res.json();
+			console.log("spec:", spec);
+
+			Object.assign(this, spec);
+
+			this.features = Array(spec.latents_dimensions).fill(0);
+		},
 	};
 </script>
 
