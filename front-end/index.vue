5e5c8e5a5332d8e8e5371fea9bb44b85c5cc01fc
k-l-lambda <k.l.lambda@gmail.com>
2019-03-12 23:04:23 +0800

    index.vue: psi & latents controls added.

--- front-end/index.vue
+++ front-end/index.vue
@@ -1,10 +1,40 @@
 <template>
 	<body>
-		<img v-if="latentsBytes" :src="`/generate?psi=${psi}&latents=${latentsBytes}`" />
+		<header>
+			&Psi; <input type="range" v-model.lazy="psi" :min="-2" :max="2" step="any" :style="{width: '600px'}" /> <em class="value">{{psi}}</em>
+		</header>
+		<aside>
+			<ul v-if="features">
+				<li v-for="(feature, index) of features" :key="index">
+					<input type="range" v-model.lazy="feature.normalized" :min="-1" :max="1" step="any" /> <em class="value">{{feature.value.toPrecision(4)}}</em>
+				</li>
+			</ul>
+		</aside>
+		<article>
+			<img v-if="latentsBytes" :src="`/generate?psi=${psi}&latents=${latentsBytes}`" />
+		</article>
 	</body>
 </template>
 
 <script>
+	class Feature {
+		constructor (value) {
+			this.value = value;
+		}
+
+
+		get normalized () {
+			return Math.tanh(this.value);
+		}
+
+
+		set normalized (v) {
+			this.value = Math.atanh(v);
+		}
+	};
+
+
+
 	export default {
 		name: "index",
 
@@ -24,7 +54,7 @@
 				if (!this.features)
 					return null;
 
-				return btoa(String.fromCharCode.apply(null, new Uint8Array(new Float32Array(this.features).buffer)));
+				return btoa(String.fromCharCode.apply(null, new Uint8Array(new Float32Array(this.features.map(f => f.value)).buffer)));
 			},
 		},
 
@@ -38,11 +68,20 @@
 
 			Object.assign(this, spec);
 
-			this.features = Array(spec.latents_dimensions).fill(0);
+			this.features = Array(spec.latents_dimensions).fill().map(() => new Feature(0));
 		},
 	};
 </script>
 
 <style>
+	aside, article
+	{
+		display: inline-block;
+		vertical-align: top;
+	}
 
+	.value
+	{
+		font-size: 9px;
+	}
 </style>
