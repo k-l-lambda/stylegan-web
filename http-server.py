84b3beaf998ae2d76dd25e79651597b48464e62d
k-l-lambda <k.l.lambda@gmail.com>
2019-12-21 20:21:15 +0800

    http-server.py: use single thread Flask to fix tensorflow variable missing issue.

--- http-server.py
+++ http-server.py
@@ -11,6 +11,8 @@ import base64
 import struct
 import numpy as np
 import tensorflow as tf
+from threading import Lock
+#import threading
 
 import dnnlib.tflib
 
@@ -21,36 +23,39 @@ load_dotenv()
 
 g_Model = None
 g_Session = None
+g_LoadingMutex = Lock()
 
 
 def loadModel():
-	global g_Model
-	if g_Model:
-		return g_Model
+	with g_LoadingMutex:
+		global g_Model
+		if g_Model:
+			return g_Model
 
-	global model_name
+		global model_name
 
-	model_path = os.environ.get('MODEL_PATH_%s' % model_name)
+		model_path = os.environ.get('MODEL_PATH_%s' % model_name)
 
-	if model_path is None:
-		print('invalid model name:', model_path)
-		return
+		if model_path is None:
+			print('invalid model name:', model_path)
+			return
 
-	print('Initializing TensorFlow...')
+		global g_Session
+		if g_Session is None:
+			print('Initializing dnnlib...')
+			dnnlib.tflib.init_tf()
+			g_Session = tf.get_default_session()
 
-	dnnlib.tflib.init_tf()
+		print('Loading model %s ...' % model_name)
 
-	global g_Session
-	g_Session = tf.get_default_session()
+		with open(model_path, 'rb') as f:
+			with g_Session.as_default():
+				Gi, Di, Gs = pickle.load(f)
+				g_Model = Gs
 
-	#global g_Session
-	#print('g_Session:', g_Session)
-	#with g_Session.as_default():
-	print('Loading model %s ...' % model_name)
-
-	with open(model_path, 'rb') as f:
-		Gi, Di, Gs = pickle.load(f)
-		g_Model = Gs
+				## burn one
+				#images = Gs.run(np.zeros((1, Gs.input_shape[1])), None, truncation_psi = 0.7, output_transform = dict(func = dnnlib.tflib.convert_images_to_uint8, nchw_to_nhwc = True))
+				#print('images:', g_Session, images)
 
 	return g_Model
 
@@ -65,6 +70,7 @@ def root():
 
 @app.route('/spec', methods=['GET'])
 def spec():
+	#print('spec.pid:', threading.get_ident())
 	global model_name
 	model = loadModel()
 
@@ -73,9 +79,10 @@ def spec():
 
 @app.route('/generate', methods=['GET'])
 def generate():
+	#print('generate.pid:', threading.get_ident())
 	latentsStr = flask.request.args.get('latents')
 	psi = float(flask.request.args.get('psi', 0.5))
-	use_noise = bool(flask.request.args.get('use_noise', True))
+	#use_noise = bool(flask.request.args.get('use_noise', True))
 	randomize_noise = bool(flask.request.args.get('randomize_noise', True))
 
 	global g_Session
@@ -90,11 +97,9 @@ def generate():
 
 	# Generate image.
 	fmt = dict(func = dnnlib.tflib.convert_images_to_uint8, nchw_to_nhwc = True)
-	#rnd = np.random.RandomState(4)
-	#latents = rnd.randn(1, model.input_shape[1])
-	print('parameters:', latents, psi, use_noise, randomize_noise, fmt)
+	#print('parameters:', latents, psi, use_noise, randomize_noise, fmt)
 	with g_Session.as_default():
-		images = model.run(latents, None, truncation_psi = psi, use_noise = use_noise, randomize_noise = randomize_noise, output_transform = fmt)
+		images = model.run(latents, None, truncation_psi = psi, randomize_noise = randomize_noise, output_transform = fmt)
 
 	print('generation cost:', time.time() - t0)
 
@@ -109,40 +114,8 @@ def main(argv):
 	global model_name
 	model_name = argv[1] if len(argv) > 1 else os.environ.get('MODEL_NAME')
 
-
-	#global g_Session
-	#with g_Session.as_default():
-	#dnnlib.tflib.init_tf()
-
-	#global g_Session
-	#g_Session = tf.get_default_session()
-	#print('g_Session.0:', g_Session)
-
-	'''	model_path = os.environ.get('MODEL_PATH_%s' % model_name)
-
-	if model_path is None:
-		print('invalid model name:', model_path)
-		return
-
-	print('Initializing TensorFlow...')
-
-	dnnlib.tflib.init_tf()
-
-	print('Loading model %s ...' % model_name)
-
-	with open(model_path, 'rb') as f:
-		global Gs, Gi, Di
-		Gi, Di, Gs = pickle.load(f)
-
-	images = test(1)
-	print('images:', images)
-
-	images2 = test(2)
-	print('images2:', images2)
-	'''
-
 	try:
-		app.run(port = int(os.getenv('HTTP_PORT')), host = os.getenv('HTTP_HOST'))
+		app.run(port = int(os.getenv('HTTP_PORT')), host = os.getenv('HTTP_HOST'), threaded = False)
 	except:
 		print('server interrupted:', sys.exc_info())
 
