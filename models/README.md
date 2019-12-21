1a7279b19640fd8de157dbc93bbcee65e188e6e1
k-l-lambda <k.l.lambda@gmail.com>
2019-12-21 15:31:52 +0800

    test.py: image generation for StyleGAN2 implemented.

---
+++ models/README.md
@@ -0,0 +1,2 @@
+
+This folder contains .pkl model files from [StyleGAN2](https://github.com/NVlabs/stylegan2), reference [this source code](https://github.com/NVlabs/stylegan2/blob/master/pretrained_networks.py).
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 ﻿
-"""Minimal script for generating an image using pre-trained StyleGAN generator."""
+"""Minimal script for generating an image using pre-trained StyleGAN2 generator."""
 
 import os
 import pickle
@@ -7,60 +7,58 @@ import numpy as np
 import PIL.Image
 import dnnlib
 import dnnlib.tflib as tflib
-import config
 import time
 
+
+
 def main():
-    t0 = time.time()
-    print('t0:', t0)
+	t0 = time.time()
+	print('t0:', t0)
 
-    # Initialize TensorFlow.
-    tflib.init_tf()     # 1.46s
+	# Initialize TensorFlow.
+	tflib.init_tf()	 # 0.82s
 
-    print('t1:', time.time() - t0)
+	print('t1:', time.time() - t0)
 
-    # Load pre-trained network.
-    url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
-    with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
-        #print('f:', f)
-        print('t2:', time.time() - t0)
+	# Load pre-trained network.
+	with open('./models/stylegan2-ffhq-config-f.pkl', 'rb') as f:
+		#print('f:', f)
+		print('t2:', time.time() - t0)
 
-        _G, _D, Gs = pickle.load(f)     # 6.8s
-        # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
-        # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
-        # Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
+		_G, _D, Gs = pickle.load(f)	 # 13.09s
+		# _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
+		# _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
+		# Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
 
-        print('t3:', time.time() - t0)
+		print('t3:', time.time() - t0)
 
-    # Print network details.
-    #Gs.print_layers()       # 0.13s
+	# Print network details.
+	#Gs.print_layers()
 
-    print('t4:', time.time() - t0)
+	print('t4:', time.time() - t0)
 
-    # Pick latent vector.
-    rnd = np.random.RandomState(4)
-    latents = rnd.randn(1, Gs.input_shape[1])
+	# Pick latent vector.
+	rnd = np.random.RandomState(4)
+	latents = rnd.randn(1, Gs.input_shape[1])
 
-    print('t5:', time.time() - t0)
+	print('t5:', time.time() - t0)
 
-    print('input_shape:', Gs.input_shape)
-    #print('latents:', latents)
+	print('input_shape:', Gs.input_shape)
+	#print('latents:', latents)
 
-    # Generate image.
-    fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)       # 0.016s
-    print('t6:', time.time() - t0)
-    images = Gs.run(latents, None, truncation_psi=-0.5, randomize_noise=True, output_transform=fmt)     # 4.96s
+	# Generate image.
+	fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)	   # 0.00s
+	print('t6:', time.time() - t0)
+	images = Gs.run(latents, None, truncation_psi=-0.5, randomize_noise=True, output_transform=fmt)	 # 6.95s
 
-    print('images:', images)
+	print('images:', images)
 
-    print('t7:', time.time() - t0)
+	print('t7:', time.time() - t0)
 
-    # Save image.
-    os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'example.png')
-    PIL.Image.fromarray(images[0], 'RGB').save(png_filename)        # 0.32s
+	# Save image.
+	PIL.Image.fromarray(images[0], 'RGB').save('./example.png')		# 0.60s
 
-    print('t8:', time.time() - t0)
+	print('t8:', time.time() - t0)
 
 if __name__ == "__main__":
-    main()
+	main()
