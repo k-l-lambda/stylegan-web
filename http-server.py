1fca838d9d49971480b9025490fefd96c4ce04c0
xunan <xunan@findpiano.cn>
2019-12-23 17:20:33 +0800

    http-server.py: handler of project added.

--- http-server.py
+++ http-server.py
@@ -14,22 +14,26 @@ import tensorflow as tf
 from threading import Lock
 
 import dnnlib.tflib
+from training import misc
+
 
 
 load_dotenv(dotenv_path = './.env.local')
 load_dotenv()
 
 
-g_Model = None
+g_Gs = None
+g_Lpips = None
+g_Projector = None
 g_Session = None
 g_LoadingMutex = Lock()
 
 
-def loadModel():
+def loadGs():
 	with g_LoadingMutex:
-		global g_Model
-		if g_Model:
-			return g_Model
+		global g_Gs
+		if g_Gs:
+			return g_Gs
 
 		global model_name
 
@@ -50,13 +54,54 @@ def loadModel():
 		with open(model_path, 'rb') as f:
 			with g_Session.as_default():
 				Gi, Di, Gs = pickle.load(f)
-				g_Model = Gs
+				g_Gs = Gs
 
 				## burn one
 				#images = Gs.run(np.zeros((1, Gs.input_shape[1])), None, truncation_psi = 0.7, output_transform = dict(func = dnnlib.tflib.convert_images_to_uint8, nchw_to_nhwc = True))
 				#print('images:', g_Session, images)
 
-	return g_Model
+	return g_Gs
+
+
+def loadLpips():
+	with g_LoadingMutex:
+		global g_Lpips
+		if g_Lpips:
+			return g_Lpips
+
+		model_path = os.environ.get('MODEL_PATH_LPIPS')
+
+		if model_path is None:
+			print('invalid model name:', model_path)
+			return
+
+		global g_Session
+		if g_Session is None:
+			print('Initializing dnnlib...')
+			dnnlib.tflib.init_tf()
+			g_Session = tf.get_default_session()
+
+		print('Loading model %s ...' % model_name)
+
+		with open(model_path, 'rb') as f:
+			with g_Session.as_default():
+				lpips = pickle.load(f)
+				g_Lpips = lpips
+
+	return g_Lpips
+
+
+def loadProjector():
+	if g_Projector:
+		return g_Projector
+
+	gs = loadGs()
+	lpips = loadLpips()
+
+	g_Projector = Projector()
+	g_Projector.set_network(gs, lpips)
+
+	return g_Projector
 
 
 app = flask.Flask(__name__, static_url_path = '', static_folder = './dist')
@@ -70,7 +115,7 @@ def root():
 @app.route('/spec', methods=['GET'])
 def spec():
 	global model_name
-	model = loadModel()
+	model = loadGs()
 
 	return dict(model = model_name, latents_dimensions = model.input_shape[1])
 
@@ -85,7 +130,7 @@ def generate():
 	global g_Session
 	print('g_Session.1:', g_Session)
 
-	model = loadModel()
+	model = loadGs()
 
 	latent_len = model.input_shape[1]
 	latents = np.array(struct.unpack('f' * latent_len, base64.b64decode(latentsStr))).reshape([1, latent_len])
@@ -104,6 +149,43 @@ def generate():
 	PIL.Image.fromarray(images[0], 'RGB').save(fp, PIL.Image.registered_extensions()['.png'])
 
 	return flask.Response(fp.getvalue(), mimetype = 'image/png')
+
+
+@app.route('/project', methods=['POST'])
+def project():
+	steps = int(flask.request.args.get('steps', 1000))
+	yieldInterval = int(flask.request.args.get('yieldInterval', 10))
+
+	imageFile = flask.request.files.get('image')
+	if not imageFile:
+		flask.abort(400, 'image field is requested.')
+
+	image = PIL.Image.open(imageFile.stream)
+	#image.save('./test.png')
+
+	image_array = np.array(image)[:, :, :3].swapaxes(0, 2).swapaxes(1, 2)
+	image_array = misc.adjust_dynamic_range(image_array, [0, 255], [-1, 1])
+
+	#print('shape:', image_array.shape)
+
+	def gen():
+		proj = loadProjector()
+		proj.start([image_array])
+		for step in proj.runSteps(steps):
+			#print('\rstep: %d' % step, end = '', flush = True)
+			if step % yieldInterval == 0:
+				dlatents = proj.get_dlatents()
+				images = proj.get_images()
+				pilImage = misc.convert_to_pil_image(misc.create_image_grid(images), drange = [-1,1])
+				fp = io.BytesIO()
+				PIL.Image.fromarray(pilImage, 'RGB').save(fp, PIL.Image.registered_extensions()['.png'])
+
+				imgUrl = 'data:image/png;base64,' % base64.b64encode(fp.getvalue())
+				latentCodes = map(lambda latents: base64.b64encode(struct.pack('f' * latents.shape[0])), dlatents.reshape((-1, dlatents[2])))
+
+				return dict(img = imgUrl, latentCodes = latentCodes)
+
+	return flask.Response(gen(), mimetype='json')
 
 
 def main(argv):
