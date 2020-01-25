10e420139353a9b52e244b0cf97a2dfa050977df
k-l-lambda <k.l.lambda@gmail.com>
2020-01-25 15:47:26 +0800

    circlePlot.vue: plot circle sliced 3 dimensions.

--- app/circlePlot.vue
+++ app/circlePlot.vue
@@ -4,6 +4,10 @@
 </template>
 
 <script>
+	import {randn_bm} from "./latentCode.js";
+
+
+
 	export default {
 		name: "mappingViewer",
 
@@ -16,6 +20,29 @@
 					height: 400,
 				}),
 			},
+			center: Float32Array,
+			circle: Array,
+		},
+
+
+		data () {
+			return {
+				normal: this.center.map(() => randn_bm()),
+			};
+		},
+
+
+		computed: {
+			normalPoints () {
+				if (!this.circle)
+					return null;
+
+				return this.circle.map(p => {
+					const v = Array.from(p).map((x, i) => x - this.center[i]);
+
+					return v;
+				});
+			},
 		},
 
 
@@ -25,45 +52,47 @@
 
 
 		mounted () {
-			const pointCount = 31;
+			this.updatePlot();
+		},
 
-			const x = [];
-			const y = [];
-			const z = [];
-			const c = [];
 
-			for (let i = 0; i < pointCount; i++) {
-				const r = 10 * Math.cos(i / 10);
-				x.push(r * Math.cos(i));
-				y.push(r * Math.sin(i));
-				z.push(i);
-				c.push(i);
-			}
+		methods: {
+			updatePlot () {
+				const x = this.normalPoints.map(v => v[0]);
+				const y = this.normalPoints.map(v => v[1]);
+				const z = this.normalPoints.map(v => v[2]);
+				const color = this.normalPoints.map((_, i) => i);
 
-			Plotly.newPlot(this.$el, [
-				{
-					type: "scatter3d",
-					mode: "lines+markers",
-					x: x,
-					y: y,
-					z: z,
-					line: {
-						width: 6,
-						color: c,
-						//colorscale: "Viridis"
-					},
-					marker: {
-						size: 3.5,
-						color: c,
-						colorscale: "Greens",
-						cmin: -20,
-						cmax: 50,
-					},
-				}
-			], {
-				margin: {l: 0, r: 0, t: 0, b: 0},
-				...this.layout,
-			});
-		}
+				Plotly.newPlot(this.$el, [
+					{
+						type: "scatter3d",
+						mode: "lines+markers",
+						x,
+						y,
+						z,
+						line: {
+							width: 6,
+							color,
+							//colorscale: "Viridis"
+						},
+						marker: {
+							size: 3.5,
+							color,
+							colorscale: "Greens",
+							cmin: -20,
+							cmax: 50,
+						},
+					}
+				], {
+					margin: {l: 0, r: 0, t: 0, b: 0},
+					...this.layout,
+				});
+			},
+		},
+
+
+		watch: {
+			normalPoints: "updatePlot",
+		},
 	};
 </script>
