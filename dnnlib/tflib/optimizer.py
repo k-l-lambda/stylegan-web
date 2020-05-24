9001d25611f9dd8315ef1a256eb8e5c38b369ca2
k-l-lambda <k.l.lambda@gmail.com>
2020-05-24 09:59:02 +0800

    dnnlib/tflib/optimizer.py: replace tf by tf.compat.v1 in obj path name.

--- dnnlib/tflib/optimizer.py
+++ dnnlib/tflib/optimizer.py
@@ -39,7 +39,7 @@ class Optimizer:
 
     def __init__(self,
         name:                   str             = "Train",                  # Name string that will appear in TensorFlow graph.
-        tf_optimizer:           str             = "tf.train.AdamOptimizer", # Underlying optimizer class.
+        tf_optimizer:           str             = "tf.compat.v1.train.AdamOptimizer", # Underlying optimizer class.
         learning_rate:          TfExpressionEx  = 0.001,                    # Learning rate. Can vary over time.
         minibatch_multiplier:   TfExpressionEx  = None,                     # Treat N consecutive minibatches as one by accumulating gradients.
         share:                  "Optimizer"     = None,                     # Share internal state with a previously created optimizer?
